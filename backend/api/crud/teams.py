0d9552b361f3d77135d5ad63b2682740003be46d
Daniel <dfishbein@mavs.coloradomesa.edu>
2024-09-20 12:37:25 -0600

    added more functionality to create sport operation

--- backend/api/crud/teams.py
+++ backend/api/crud/teams.py
@@ -2,7 +2,8 @@ from typing import List, Any, Dict
 from bson.objectid import ObjectId
 from api.utils.dependencies import get_database
 from api.utils.json_helper import json_file_builder
-from api.config import LEVEL_CONSTANTS
+from api.utils.update_algo_vals import update_values
+# from api.config import LEVEL_CONSTANTS
 
 db = get_database()
 teams_collection = db.get_collection("sports")
@@ -22,31 +23,12 @@ async def create_sport(
     )
 
     if algo_vals:
-        sport_json_doc['sports'][sport_type][gender][level]['k_value'] = \
-            algo_vals.get(
-                "k_value",
-                LEVEL_CONSTANTS[
-                    (sport_type, gender, level): ['k_value']
-                ]
-            )
-        sport_json_doc['sports'][sport_type][gender][level]['home_advantage'] \
-            = algo_vals.get(
-                "home_advantage",
-                LEVEL_CONSTANTS[
-                    (sport_type, gender, level): ['home_advantage']
-                ]
-            )
-        sport_json_doc['sports'][sport_type][gender][level]['average_game_score'] \
-            = algo_vals.get(
-                "average_game_score",
-                LEVEL_CONSTANTS[
-                    (sport_type, gender, level): ['average_game_score']
-                ]
-            )
-        sport_json_doc['sports'][sport_type][gender][level]['game_set_len'] \
-            = algo_vals.get(
-                "game_set_len",
-                LEVEL_CONSTANTS[
-                    (sport_type, gender, level): ['game_set_len']
-                ]
-            )
+        # Function to update algorithm values if need be otherwise
+        # they stay there constant value
+
+        level_key = (sport_type, gender, level)
+        sport_json_doc = update_values(
+            level_key=level_key,
+            sport_json_doc=sport_json_doc,
+            algo_vals=algo_vals
+        )
