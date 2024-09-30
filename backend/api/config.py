b8dfd26b9f3fb34adfa3e6baa75b6fbc2d65cd7c
Daniel <dfishbein@mavs.coloradomesa.edu>
2024-09-29 23:55:05 -0600

    updated values to correct algorithm values

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,38 +1,40 @@
 LEVEL_CONSTANTS = {
     ("football", "mens", "high_school"): {
-        "k_value": 0.0,
-        "home_advantage": 0,
-        "average_game_score": 0,
-        "game_set_len": 0
+        "k_value": 0.7,
+        "home_advantage": 3,
+        "average_game_score": 50,
+        "game_set_len": 7
     },
     ("football", "mens", "college"): {
-        "k_value": 0.0,
-        "home_advantage": 0,
-        "average_game_score": 0,
-        "game_set_len": 0
+        "k_value": 0.69,
+        "home_advantage": 3,
+        "average_game_score": 60,
+        "game_set_len": 7
     },
     ("basketball", "mens", "high_school"): {
-        "k_value": 0.0,
-        "home_advantage": 0,
-        "average_game_score": 0,
-        "game_set_len": 0
+        "k_value": 0.43,
+        "home_advantage": 4.5,
+        "average_game_score": 106,
+        "game_set_len": 7
     },
     ("basketball", "mens", "college"): {
-        "k_value": 0.0,
-        "home_advantage": 0,
-        "average_game_score": 0,
-        "game_set_len": 0
+        "k_value": 0.31,
+        "home_advantage": 4.5,
+        "average_game_score": 136,
+        "game_set_len": 1
     },
     ("basketball", "womens", "high_school"): {
-        "k_value": 0.0,
-        "home_advantage": 0,
-        "average_game_score": 0,
-        "game_set_len": 0
+        "k_value": 0.43,
+        "home_advantage": 4.5,
+        "average_game_score": 83,
+        "game_set_len": 7
     },
     ("basketball", "womens", "college"): {
-        "k_value": 0.0,
-        "home_advantage": 0,
-        "average_game_score": 0,
-        "game_set_len": 0
+        "k_value": 0.31,
+        "home_advantage": 4.5,
+        "average_game_score": 122,
+        "game_set_len": 1
     }
 }
+
+
