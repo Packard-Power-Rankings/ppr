14efcd1cfb980936cb28961986a895cbbe532875
Daniel <dfishbein@mavs.coloradomesa.edu>
2024-10-03 23:32:34 -0600

    changed a couple of things, just small things and some fucntion overiding

--- backend/api/routers/teams.py
+++ backend/api/routers/teams.py
@@ -54,39 +54,42 @@ async def add_sports(
     sport_doc = jsonable_encoder(sport_doc)
     # Below I need the service connection for the database
     new_sports = await add_sports_data(sport_doc)
-    return items.ResponseModel(new_sports, "Succesfully added new sports")
+    return items.ResponseModel(new_sports, "Successfully added new sports")
 
 
 # READ routes:
 @router.get("/sports/teams/{id}", response_description="Display sports data")
 async def list_sports(
-    id: int,
+    sport_id: int,
     sport_type: str,
     gender: str,
     level: str,
 ):
     try:
         # Create the query based on the provided parameters
-        query: Dict = {"id": id}  # Initialize with ID as it's mandatory
+        query: Dict = {"id": sport_id}  # Initialize with ID as it's mandatory
 
         # Generate sport_doc just like in the POST route
         sport_doc: Dict = json_file_builder(sport_type, gender, level)
 
         # Fetch sports data using the query
-        sports = await retrieve_sports(query, id)
+        sports = await retrieve_sports(query, sport_id)
 
         # Check if sports data is found
         if sports:
-            team_info = sport_doc['sports'][sport_type][gender][level].get('team', [])
-            
+            team_info = \
+                sport_doc['sports'][sport_type][gender][level].get('team', [])
+
             response_data = {
                 "sport": sport_doc,
                 "teams": team_info,
                 "sports_data": sports
             }
-            return items.ResponseModel(response_data, "Sports data retrieved successfully")
-        else:
-            return items.ResponseModel([], "No sports data found")
-        
+            return items.ResponseModel(
+                response_data,
+                "Sports data retrieved successfully"
+            )
+        return items.ResponseModel([], "No sports data found")
+
     except Exception as e:
         return {"error": f"An error occurred: {str(e)}"}
