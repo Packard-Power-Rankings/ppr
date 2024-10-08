f489454cea3df373f2219b19a0b6a24642f73754
Daniel <dfishbein@mavs.coloradomesa.edu>
2024-10-08 10:25:56 -0600

    updated helper function

--- backend/api/utils/update_algo_vals.py
+++ backend/api/utils/update_algo_vals.py
@@ -13,20 +13,16 @@ from config import LEVEL_CONSTANTS
 
 def update_values(
         level_key: Tuple,
-        algo_vals: Dict) -> Any:
+        algo_vals: Dict):
     """
-        : Updates Algorithm values if any are passed through
-        : and uses defaults that are set in LEVEL_CONSTANTS. Also
-        : it will update values in LEVEL_CONSTANTS as well.
+        : Function to check if the values that the main algorithm
+        : needs to do calculations were needing to be updated. If
+        : so, then check if value it is non-falsy and not a matching
+        : value.
 
     Args:
         level_key (Tuple): Values used to search in LEVEL_CONSTANTS
-        sport_json_doc (Any): JSON file to be updated
-        algo_vals (Dict): Keyword dictionary from create sports
-
-    Returns:
-        JSON: Returns an updated JSON file with the updated algorithm
-        values stored appropriately.
+        algo_vals (Dict): Dictionary from create sports
     """
 
     params = [
@@ -36,7 +32,8 @@ def update_values(
         'game_set_len'
     ]
     for param in params:
-        new_value = algo_vals.get(param, LEVEL_CONSTANTS[level_key][param])
+        replacement_value = algo_vals.get(param)
+        lvl_check = LEVEL_CONSTANTS[level_key][param]
 
-        if param in algo_vals:
-            LEVEL_CONSTANTS[level_key][param] = new_value
+        if replacement_value and replacement_value != lvl_check:
+            LEVEL_CONSTANTS[level_key][param] = replacement_value
