842fbc819cc0f5962a1b4c5171314f7cdf591813
Daniel <dfishbein@mavs.coloradomesa.edu>
2024-10-25 00:06:14 -0600

    creating classes with all the logic for the databaseses querying instead of in routers

---
+++ backend/api/service/admin_service.py
@@ -0,0 +1,139 @@
+"""Admin service class that creates a single admin,
+    generates password hash and stores in database, checks
+    if password is correct, and genrates an access token
+
+    Raises:
+        HTTPException: 401 Unathorized Access
+        HTTPException: 500 Server Error
+        HTTPException: 404 Not Found
+        HTTPException: 400 Bad Request
+        credentials_exception: 401 Unathorized Access
+
+    Returns:
+        None
+"""
+
+import os
+from typing import Annotated
+from datetime import datetime, timedelta, timezone
+import motor.motor_asyncio
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+import bcrypt
+import jwt
+from jwt.exceptions import InvalidTokenError
+from schemas.items import TokenData
+
+ACCESS_TOKEN_TIME = 60.0
+ALGORITHM = "HS256"
+MONGO_DETAILS = \
+    f"mongodb+srv://{os.getenv("MONGO_USER")}:{os.getenv("MONGO_PASS")}@" \
+    "sports-cluster.mx1mo.mongodb.net/" \
+    "?retryWrites=true&w=majority&appName=Sports-Cluster"
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+database = client['admin_details']
+admin = database.get_collection('admin')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class AdminServices():
+    def __init__(self):
+        self.admin_collection = admin
+
+    async def create_admin(self, username: str, password: str) -> str:
+        try:
+            exsisting_admin = await self.admin_collection.find_one({})
+            if exsisting_admin:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Admin Already Exists"
+                )
+            hashed_pass = self.hashed_password(password)
+            new_admin = {
+                "username": username,
+                "password": hashed_pass
+            }
+            result = await self.admin_collection.insert_one(new_admin)
+            return str(result.inserted_id)
+        except Exception as exc:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Error has occured"
+            ) from exc
+
+    async def verify_admin(self, username: str, password: str) -> str:
+        admin = self.admin_collection.find_one({"username": username})
+        if not admin:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Admin not found"
+            )
+
+        hash_pass = admin["password"]
+        if self.check_password(password, hash_pass):
+            return self.generate_access_token({"sub": username}, None)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid Password"
+        )
+
+    async def get_current_user(
+        self,
+        token: Annotated[str, Depends(oauth2_scheme)]
+    ):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+        try:
+            payload = jwt.decode(
+                token,
+                os.getenv("SECRET_KEY"),
+                algorithms=[ALGORITHM]
+            )
+            username: str = payload.get('sub')
+            if username is None:
+                raise credentials_exception
+            return TokenData(username=username)
+        except InvalidTokenError:
+            raise credentials_exception
+        
+    def generate_access_token(
+        self,
+        data: dict,
+        expires_delta = timedelta | None = None
+    ) -> str:
+        to_encode = data.copy()
+        if expires_delta:
+            expire = \
+                datetime.now(timezone.utc) \
+                    + expires_delta
+        else:
+            expire = \
+                datetime.now(timezone.utc) \
+                    + timedelta(minutes=ACCESS_TOKEN_TIME)
+
+        to_encode.update({'exp': expire})
+        encode_jwt = jwt.encode(
+            to_encode,
+            os.getenv('SECRET_KEY'),
+            algorithm=ALGORITHM
+        )
+        return encode_jwt
+
+    @staticmethod
+    def hashed_password(password: str) -> str:
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(
+            password=password.encode('utf-8'),
+            salt=salt
+        ).decode('utf-8')
+
+    @staticmethod
+    def check_password(submitted_pass: str, hashed_pass: str) -> bool:
+        return bcrypt.checkpw(
+            submitted_pass.encode("utf-8"),
+            hashed_pass.encode('utf-8')
+        )
---
+++ backend/api/service/admin_teams.py
@@ -0,0 +1,175 @@
+import os
+from io import StringIO
+from typing import Any, List, Dict, Tuple
+import traceback
+import csv
+from datetime import datetime
+from fastapi import HTTPException, status, UploadFile
+from bson.binary import Binary
+import motor.motor_asyncio
+from config.config import LEVEL_CONSTANTS
+from schemas import items
+
+MONGO_DETAILS = \
+    f"mongodb+srv://{os.getenv("MONGO_USER")}:{os.getenv("MONGO_PASS")}@" \
+    "sports-cluster.mx1mo.mongodb.net/" \
+    "?retryWrites=true&w=majority&appName=Sports-Cluster"
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+database = client["sports_data"]
+
+class AdminTeamsService():
+    def __init__(self, level_key: Tuple):
+        self.sports_collection = database.get_collection('teams_data')
+        self.csv_collection = database.get_collection('csv_files')
+        self.level_constant = LEVEL_CONSTANTS[level_key]
+
+    async def store_csv_check_teams(
+        self,
+        sport_type: str,
+        gender: str,
+        level: str,
+        csv_file: UploadFile
+    ):
+        """Admin level http post method
+
+        Args:
+            sport_type (str): Type Of Sport
+            gender (str): Sport Gender
+            level (str): Sport Level
+            csv_file (UploadFile): CSV File to Store
+
+        Raises:
+            HTTPException: 400 Bad Request
+            HTTPException: 404 Not Found
+
+        Returns:
+            dict: Message for succesfull upload and an array of
+            missing teams (if any).
+        """
+        try:
+            file_name = csv_file.filename
+            file_content = await csv_file.read()
+            query_csv = {
+                "sport_type": sport_type,
+                "gender": gender,
+                "level": level
+            }
+            file_upload = await self._add_csv_file(
+                query_csv,
+                file_name,
+                file_content
+            )
+
+            decode_content = file_content.decode("utf-8")
+            csv_reader = csv.reader(StringIO(decode_content))
+
+            team_check: List[str] = []
+            for team in csv_reader:
+                team_check.append(team[1].lower())
+                team_check.append(team[2].lower())
+
+            query_teams = {
+                "_id": self.level_constant.get('_id')
+            }
+            results = await self._find_teams(query_teams, team_check)
+
+            if results:
+                teams: List[Dict[str, str]] = results[0].get('teams')
+                for team in teams:
+                    if team.get('team_name').lower() in \
+                        [t.lower() for t in team_check]:
+                            team_check = [
+                                t for t in team_check 
+                                if t.lower() != team.get('team_name').lower()
+                            ]
+                csv_file.close()
+                return {
+                    "success": "File Uploaded and Teams Searched",
+                    "status": status.HTTP_200_OK,
+                    "missing_teams": team_check,
+                    "files_uploaded": file_upload
+                }
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Error in team checking"
+            )
+    
+        except Exception as exc:
+            traceback.print_exc()
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="An error has occurred"
+            ) from exc
+
+    async def add_teams_to_db(
+        self,
+        teams: list[items.NewTeam],
+        number_of_runs: int
+    ) -> Any:
+        if teams:
+            for team in teams:
+                team_id = await self._generate_team_id()
+                new_team_data = {
+                    "team_id": team_id,
+                    "team_name": team.team_name,
+                    "city": "",
+                    "division": team.division,
+                    "conference": team.conference,
+                    "division_rank": 0,
+                    "overall_rank": 0,
+                    "power_rankings": [team.power_ranking],
+                    "win_ratio": 0.0,
+                    "wins": 0,
+                    "losses": 0,
+                    "recent_opp": [0, 0, 0, 0, 0],
+                    "season_opp": []
+                }
+                results = await self.sports_collection.update_one(
+                    {self.level_constant.get('_id')},
+                    {"teams": {"$push": new_team_data}},
+                    upsert=True
+                )
+            return {
+                "success": "Teams were added and algorithm was ran",
+                "status": status.HTTP_200_OK,
+                "number_of_files": results.modified_count
+            }
+
+    async def _add_csv_file(
+        self,
+        query: dict,
+        filename: str,
+        csv_file: Any
+    ) -> int:
+        file_entry = {
+            "filename": filename,
+            "filedata": Binary(csv_file),
+            "upload_date": str(datetime.datetime.today())
+        }
+        results = await self.csv_collection.update_one(
+            query,
+            {"$push": {"csv_files": file_entry}},
+            upsert=True
+        )
+        return results.modified_count
+
+    async def _find_teams(self, query: dict, teams_search: list) -> list:
+        results = self.sports_collection.find(
+            {**query, "teams": {"$nin": teams_search}},
+            {"_id": 0}
+        )
+        return await results.to_list()
+
+    async def _generate_team_id(self):
+        teams = await self.sports_collection.find_one(
+            {'_id': self.level_constant.get('_id')},
+            sort=[("team_id", -1)],
+            projection={"teams": 1, "_id": 0}
+        )
+        if teams:
+            max_id = teams['team_id']
+            new_team_id = max_id + 1
+        else:
+            new_team_id = 1
+        return new_team_id
+
