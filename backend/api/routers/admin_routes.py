6a4c608b1adf57d3e4e0165a60de1213e30c02f6
Daniel <dfishbein@mavs.coloradomesa.edu>
2024-10-29 08:10:33 -0600

    small changes

--- backend/api/routers/admin_routes.py
+++ backend/api/routers/admin_routes.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 import traceback
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from fastapi import APIRouter, Depends, File, UploadFile, HTTPException, status
 from schemas.items import InputMethod, NewTeam, input_method_dependency
+from service.admin_teams import AdminTeamsService
 # from service.admin_service import AdminServices
 
 router = APIRouter()
+_instance_cache: Dict[Tuple, "AdminTeamsService"] = {}
 
-def admin_team_class(level_key: Tuple):
-    from service.admin_teams import AdminTeamsService
-    return AdminTeamsService(level_key)
+
+def admin_team_class(level_key: Tuple) -> "AdminTeamsService":
+    if level_key not in _instance_cache:
+        _instance_cache[level_key] = AdminTeamsService(level_key)
+    return _instance_cache[level_key]
+
+
+# def admin_team_class(level_key: Tuple):
+#     from service.admin_teams import AdminTeamsService
+#     return AdminTeamsService(level_key)
 
 
 @router.post("/upload_csv", tags=["Admin"])
@@ -36,12 +45,11 @@ async def upload_csv(
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Internal Error"
-        )
+        ) from exc
 
 
 @router.post("/add_teams", tags=["Admin"])
 async def add_missing_teams(
-    itter: int,
     sports_input: InputMethod = Depends(input_method_dependency)
 ):
     try:
@@ -51,14 +59,26 @@ async def add_missing_teams(
                 sports_input.level
         )
         team_services = admin_team_class(level_key)
-        results = await team_services.add_teams_to_db([], itter)
+        tmp_team = {
+                "team_name": "Sul Ross",
+                "division": None,
+                "conference": None,
+                "power_ranking": 286.53863361385584,
+                "state": "Texas"
+        }
+        results = await team_services.add_teams_to_db(tmp_team)
         return results
     except Exception as exc:
         traceback.print_exc()
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Internal Error"
-        )
+        ) from exc
+
+
+@router.post("/run_algorithm", tags=["Admin"])
+async def main_algorithm_exc(iterations: int):
+    pass
 
 
 @router.put("/update_game", tags=["Admin"])
