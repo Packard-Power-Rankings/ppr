9a7749cacc7df9e8a149530b0a78d873dbb573a3
Daniel Fishbein <danielfishbein@Daniels-MacBook-Pro.local>
2025-02-19 08:43:02 -0700

    updated the logo import

--- frontend/src/components/AppSidebar.js
+++ frontend/src/components/AppSidebar.js
@@ -14,7 +14,7 @@ import CIcon from '@coreui/icons-react'
 import { AppSidebarNav } from './AppSidebarNav'
 
 import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
+// import { sygnet } from 'src/assets/brand/sygnet'
 
 // sidebar nav config
 import navigation from '../_nav'
@@ -38,7 +38,7 @@ const AppSidebar = () => {
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
           <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          <CIcon customClassName="sidebar-brand-narrow" icon={logo} height={32} />
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
