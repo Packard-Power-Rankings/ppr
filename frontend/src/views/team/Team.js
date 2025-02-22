7aaffcb6ee04b8063c065733698596013639801c
Daniel Fishbein <danielfishbein@Daniels-MacBook-Pro.local>
2025-02-21 21:23:12 -0700

    updated team page

--- frontend/src/views/team/Team.js
+++ frontend/src/views/team/Team.js
@@ -42,7 +42,7 @@ const Team = () => {
             setError(null);
 
             const teamInfo = await api.get(`/${team_name}/${sport}/${gender}/${level}`);
-            console.log(teamInfo.data.data.teams.season_opp);
+            // console.log(teamInfo.data.data.teams.season_opp);
             setTeam(teamInfo.data.data.teams);
             setOpp(teamInfo.data.data.teams.season_opp);
         } catch (error) {
@@ -93,30 +93,32 @@ const Team = () => {
                     </CCardBody>
                 </CCard>
             </CContainer>
-            <CTable hover responsive>
+            <CTable align="middle" hover responsive>
                 <CTableHead>
                     <CTableRow>
-                        <CTableHeaderCell>Upload Date</CTableHeaderCell>
-                        <CTableHeaderCell>Opponent</CTableHeaderCell>
-                        <CTableHeaderCell>Home Game</CTableHeaderCell>
-                        <CTableHeaderCell>Home Score</CTableHeaderCell>
-                        <CTableHeaderCell>Home z-score</CTableHeaderCell>
-                        <CTableHeaderCell>Away Score</CTableHeaderCell>
-                        <CTableHeaderCell>Away z-score</CTableHeaderCell>
-                        <CTableHeaderCell>Result</CTableHeaderCell>
+                        <CTableHeaderCell className="py-3">Upload Date</CTableHeaderCell>
+                        <CTableHeaderCell className="py-3">Opponent</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center py-3">Home Game</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center py-3">Home Score</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center py-3">Home z-score</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center py-3">Away Score</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center py-3">Away z-score</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center py-3">Result</CTableHeaderCell>
                     </CTableRow>
                 </CTableHead>
                 <CTableBody>
                     {seasonOpp.map((game, index) => (
                         <CTableRow key={index}>
-                            <CTableDataCell>{game.game_date}</CTableDataCell>
+                            <CTableDataCell className="py-3">{game.game_date}</CTableDataCell>
                             <CTableDataCell>{game.opponent_name}</CTableDataCell>
-                            <CTableDataCell>{game.home_team ? 'Yes' : 'No'}</CTableDataCell>
-                            <CTableDataCell>{game.home_score}</CTableDataCell>
-                            <CTableDataCell>{game.home_z_score.toFixed(2)}</CTableDataCell>
-                            <CTableDataCell>{game.away_score}</CTableDataCell>
-                            <CTableDataCell>{game.away_z_score.toFixed(2)}</CTableDataCell>
-                            <CTableDataCell>
+                            <CTableDataCell className="text-center">
+                                {game.home_team ? 'Yes' : 'No'}
+                            </CTableDataCell>
+                            <CTableDataCell className="text-center">{game.home_score}</CTableDataCell>
+                            <CTableDataCell className="text-center">{game.home_z_score.toFixed(2)}</CTableDataCell>
+                            <CTableDataCell className="text-center">{game.away_score}</CTableDataCell>
+                            <CTableDataCell className="text-center">{game.away_z_score.toFixed(2)}</CTableDataCell>
+                            <CTableDataCell className="text-center">
                                 {game.home_score > game.away_score
                                     ? 'Win'
                                     : game.home_score < game.away_score
