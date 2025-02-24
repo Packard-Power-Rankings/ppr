c4765a0e0f158168188edf00d56f32a2594df50c
Daniel Fishbein <danielfishbein@Daniels-MacBook-Pro.local>
2025-02-24 12:36:14 -0700

    updated default layout to hide/show admin header and admin nav

--- frontend/src/layout/DefaultLayout.js
+++ frontend/src/layout/DefaultLayout.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
+import { AppContent, AppSidebar, AppFooter, AppHeader, AdminHeader } from '../components/index'
 
 const DefaultLayout = () => {
+  const isAdmin = useSelector((state) => state.isAdmin);
+  const location = useLocation();
+
+  const isAdminLocation = location.pathname.startsWith('/admin');
+
   return (
     <div>
       <AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100">
         <AppHeader />
+        {isAdmin && isAdminLocation && <AdminHeader />}
         <div className="body flex-grow-1">
           <AppContent />
         </div>
