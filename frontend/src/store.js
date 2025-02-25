bfd6e70a16ec0fb06a271b9fe1044d2eb90a0024
Daniel Fishbein <danielfishbein@Daniels-MacBook-Pro.local>
2025-02-25 13:50:26 -0700

    added sport, gender, and level to state for admin header

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,16 +4,21 @@ const initialState = {
   sidebarShow: true,
   theme: 'light',
   isAdmin: false,
+  sport: "football",
+  gender: "mens",
+  level: "high_school",
 }
 
-const changeState = (state = initialState, { type, ...rest }) => {
+const changeState = (state = initialState, { type, payload }) => {
   switch (type) {
     case 'set':
-      return { ...state, ...rest }
+      return { ...state, ...payload }
     case 'login':
       return { ...state, isAdmin: true }
     case 'logout':
       return { ...state, isAdmin: false }
+    case 'updateAdminState':
+      return { ...state, ...payload }
     default:
       return state
   }
