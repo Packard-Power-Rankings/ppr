8e8c08acf763f7e43f75fde8ff94d490d496fa36
Daniel Fishbein <danielfishbein@Daniels-MacBook-Pro.local>
2025-02-26 09:04:34 -0700

    added persist gate for app refresh

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
-import store from './store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 )
 
 // If you want to start measuring performance in your app, pass a function
